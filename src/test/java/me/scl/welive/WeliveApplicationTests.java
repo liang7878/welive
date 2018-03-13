f8b65e2bcb5074c79883cd9eff06361698b7125a
Chengliang Shi <chengliangshi@whu.edu.cn>
2018-03-13 23:49:40 +0800

    [save] first commit

---
+++ src/test/java/me/scl/welive/WeliveApplicationTests.java
@@ -0,0 +1,16 @@
+package me.scl.welive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeliveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
